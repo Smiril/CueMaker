775740f2fadfb9c86b552300fcef2fb20a3dcd99
Smiril <Smiril@users.noreply.github.com>
2024-03-22 11:03:55 +0100

    Update CueMakerPy2.py
    two track fix for cue file

--- CueMaker/CueMakerPy2/CueMakerPy2.py
+++ CueMaker/CueMakerPy2/CueMakerPy2.py
@@ -4,10 +4,14 @@
 import os
 import sys
 from os import walk
+from os.path import exists
+import re
 
+patterns1 = ['Track 1']
+patterns2 = ['Track 2']
 # Define path to search
 # mypath is a literal string so you dont have to escape path
-mypath = r"/home/pi/RetroPie/roms";
+mypath = r"./roms";
 recursive = True
 
 # Get input
@@ -16,28 +20,24 @@ if (sys.version_info > (3, 0)):
     #py3 code
     mypath = input("Please enter path to scan ["+mypath+"]:") or mypath
     trecursive = input("Search Recursive? ["+str(recursive)+"]:").strip().title() or recursive
+    filenamex = input("Please enter Project Name [filename]:")
     recursive = trecursive in trues
 else:
     #py2 code
     mypath = raw_input("Please enter path to scan ["+mypath+"]:") or mypath
     trecursive = raw_input("Search Recursive? ["+str(recursive)+"]:").strip().title() or recursive
+    filenamex = input("Please enter Project Name [filename]:")
     recursive = trecursive in trues
 
-# Get input
-rawor = ("Raw", "R", True)
-rawno = ("Truncated","T", False)
-mary = input("Please enter Raw or Truncated [R/T]:")
-if (mary, not rawno):
-    # Define cue file
-    cue = r"""FILE "%FILENAME%" BINARY
-    TRACK 01 MODE1/2352
-    INDEX 01 00:00:00"""
-else:
-    # Define cue file
-    cue = r"""FILE "%FILENAME%" BINARY
-    TRACK 01 MODE1/2336
-    INDEX 01 00:00:00"""
-
+# Define cue file
+cue1 = r"""FILE "%TRACK1%" BINARY
+TRACK 01 MODE1/2352
+INDEX 01 00:00:00"""
+cue2 = r"""
+FILE "%TRACK2%" BINARY
+TRACK 02 AUDIO
+INDEX 00 00:00:00
+INDEX 01 00:02:00"""
 
 # For each (sub)folder
 for (dirpath, dirnames, filenames) in walk(mypath):
@@ -47,20 +47,28 @@ for (dirpath, dirnames, filenames) in walk(mypath):
         filename, file_extension = os.path.splitext(file)
 
         # If its a bin
-        if file_extension.lower()==".bin" or file_extension.lower()==".iso" or file_extension.lower()==".img":
-
+        if (exists((file_extension.lower()==".bin" or file_extension.lower()==".iso" or file_extension.lower()==".img"))):
             # Search for a cue
-            if (filename + ".cue").lower() not in (name.lower() for name in filenames):
-
+            if (filenamex + ".cue").lower() not in (name.lower() for name in filenames):
                 # None found, create one
-                savePath = os.path.join(dirpath,filename+ ".cue")
-                fileHandle = open(savePath, "w")
-                fileHandle.write(cue.replace("%FILENAME%", file))
-                fileHandle.close()
-
+                savePath = os.path.join(dirpath,filenamex+ ".cue")
+                fileHandle = open(savePath, "a")
+                for i in patterns1:
+                    if re.search (i, file):
+                        fileHandle.write(cue1.replace("%TRACK1%", file))
+                    else:
+                        print("no match!")
+                        
+                for k in patterns2:
+                    if re.search (k, file):
+                        fileHandle.write(cue2.replace("%TRACK2%", file))
+                    else:
+                        print("no match!")
+                        
+            fileHandle.close()
                 # Output write
-                print("Cue created: " + savePath)
-
+            print("Cue created: " + savePath)
+                
     # If they dont want to go recursive, get out
     if not recursive:
         break
